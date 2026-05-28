480f5421ce317d586483e795aaa9a0e51cf40940
Franck PFLIEGER <54175705+FranckPFLIEGER@users.noreply.github.com>
2026-05-28 15:03:59 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Header } from '../components/Header'
-import { Hero } from '../components/Hero'
-import { Stats } from '../components/Stats'
-import { About } from '../components/About'
-import { Services } from '../components/Services'
+import { Header }         from '../components/Header'
+import { Hero }           from '../components/Hero'
+import { Stats }          from '../components/Stats'
+import { About }          from '../components/About'
+import { Clients }        from '../components/Clients'
+import { Results }        from '../components/Results'
+import { Services }       from '../components/Services'
 import { Certifications } from '../components/Certifications'
-import { WhyUs } from '../components/WhyUs'
-import { ContactCTA } from '../components/ContactCTA'
-import { Footer } from '../components/Footer'
+import { WhyUs }          from '../components/WhyUs'
+import { News }           from '../components/News'
+import { AwardsSection }  from '../components/Awards'
+import { ContactCTA }     from '../components/ContactCTA'
+import { Footer }         from '../components/Footer'
 
 export const Route = createFileRoute('/')({
   component: HomePage,
@@ -21,9 +25,13 @@ function HomePage() {
         <Hero />
         <Stats />
         <About />
+        <Clients />
+        <Results />
         <Services />
         <Certifications />
         <WhyUs />
+        <News />
+        <AwardsSection />
         <ContactCTA />
       </main>
       <Footer />

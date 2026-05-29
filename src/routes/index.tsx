1c3fba1cb48e1e12c13b66989a4a306892ae0c97
Franck PFLIEGER <54175705+FranckPFLIEGER@users.noreply.github.com>
2026-05-29 12:16:14 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,18 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Header }         from '../components/Header'
 import { Hero }           from '../components/Hero'
 import { Stats }          from '../components/Stats'
+import { President }      from '../components/President'
 import { About }          from '../components/About'
+import { Presence }       from '../components/Presence'
 import { Clients }        from '../components/Clients'
 import { Results }        from '../components/Results'
 import { Services }       from '../components/Services'
 import { Certifications } from '../components/Certifications'
 import { WhyUs }          from '../components/WhyUs'
+import { Partners }       from '../components/Partners'
 import { News }           from '../components/News'
 import { AwardsSection }  from '../components/Awards'
+import { RSE }            from '../components/RSE'
 import { ContactCTA }     from '../components/ContactCTA'
 import { Footer }         from '../components/Footer'
 
@@ -22,16 +26,43 @@ function HomePage() {
     <>
       <Header />
       <main>
+        {/* Hero */}
         <Hero />
+
+        {/* Chiffres clés */}
         <Stats />
+
+        {/* Qui Sommes-Nous */}
+        <President />
         <About />
+        <Presence />
+
+        {/* Références & Résultats */}
         <Clients />
         <Results />
+
+        {/* Nos Services */}
         <Services />
+
+        {/* Certifications / Éditeurs */}
         <Certifications />
+
+        {/* Pourquoi Nous Choisir */}
         <WhyUs />
+
+        {/* Partenaires */}
+        <Partners />
+
+        {/* Actualités */}
         <News />
+
+        {/* Awards */}
         <AwardsSection />
+
+        {/* RSE */}
+        <RSE />
+
+        {/* Contact */}
         <ContactCTA />
       </main>
       <Footer />

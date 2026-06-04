7f36f41cbc1c6348dad743230249522638455f26
Franck PFLIEGER <54175705+FranckPFLIEGER@users.noreply.github.com>
2026-06-04 17:06:37 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -188,6 +188,7 @@ const NAV_LINKS = [
       { label: 'Satisfaction & Qualité',  href: '/#satisfaction' },
       { label: 'Ils nous font confiance', href: '/#clients' },
       { label: 'Nos résultats',           href: '/#resultats' },
+      { label: 'Éditeurs officiels',       href: '/#partenaires' },
       { label: 'Démarche pédagogique',    href: '/demarche-pedagogique' },
       { label: 'Démarche qualité',        href: '/demarche-qualite' },
       { label: 'Nos partenaires',         href: '/#partenaires' },
